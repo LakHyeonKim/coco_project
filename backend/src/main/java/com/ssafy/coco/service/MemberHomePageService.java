c2e1f77f81cace96df03c705127dff6cae0c9c73
김락현 <rlafkrgus963@kumoh.ac.kr>
2020-02-05 18:55:08 +0900

    [락현] S02P13D105-57 Mypage 컨트롤러 추가(계속)

--- backend/src/main/java/com/ssafy/coco/service/MemberHomePageService.java
+++ backend/src/main/java/com/ssafy/coco/service/MemberHomePageService.java
@@ -3,5 +3,5 @@ package com.ssafy.coco.service;
 import com.ssafy.coco.relationvo.MemberHomePage;
 
 public interface MemberHomePageService {
-	public MemberHomePage findByMemberHomePageUserID(String id);
+	public MemberHomePage findByMemberHomePageUserID(long idMember);
 }
