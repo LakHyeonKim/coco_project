66bae0e04dfda9deaf49b6bba923036d166d77ff
김락현 <rlafkrgus963@kumoh.ac.kr>
2020-02-07 09:52:20 +0900

    [락현] database 파일 삭제

--- database/post_dummy_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `coco`.`post` (`member_id`, `post_title`, `post_writer`, `date_created`, `code`, `like_count`, `views`, `image_path`, `access`) VALUES ('5', '동적쿼리 작성하는 법', '진호짱짱맨', now(), '어쩌구 저쩌구', '0', '0', '어쩌구', '1');
-INSERT INTO `coco`.`post` (`member_id`, `post_title`, `post_writer`, `date_created`, `code`, `like_count`, `views`, `image_path`, `access`) VALUES ('5', '인생은 왜 외로운가', '진호짱짱맨', now(), '어쩌구 저쩌구', '0', '0', '어쩌구', '1');
-INSERT INTO `coco`.`post` (`member_id`, `post_title`, `post_writer`, `date_created`, `code`, `like_count`, `views`, `image_path`, `access`) VALUES ('5', '라이언은 왜 귀여운가', '진호짱짱맨', now(), '어쩌구 저쩌구', '0', '0', '어쩌구', '1');
-INSERT INTO `coco`.`post` (`member_id`, `post_title`, `post_writer`, `date_created`, `code`, `like_count`, `views`, `image_path`, `access`) VALUES ('5', '진호따리진호따리', '진호짱짱맨', now(), '어쩌구 저쩌구', '0', '0', '어쩌구', '1');
-
-SELECT * FROM coco.post;
