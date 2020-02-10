60a1d5fb6ab814ccad5d6cc2670f6c1512d8711b
김락현 <rlafkrgus963@kumoh.ac.kr>
2020-02-10 14:53:44 +0900

    [락현] S02P13D105-93 today reset

---
+++ backend/src/main/java/com/ssafy/coco/scheduler/TodayReset.java
@@ -0,0 +1,28 @@
+package com.ssafy.coco.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.ssafy.coco.dao.MyPageDao;
+
+@Component
+public class TodayReset {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TodayReset.class);
+	
+	@Autowired
+	private MyPageDao myPageDao;
+	
+	@Scheduled(cron="0 */5 * * * *") // "* * * 1 * *"
+	public void todayCountReset() {
+		logger.info("all member page reset");
+		try {
+			myPageDao.updateMypageTodayVisitedReset();			
+		}catch(Exception ex){
+            logger.error(ex.getMessage());
+        }
+	}
+}
