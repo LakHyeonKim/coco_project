2aa8f64ac19fc318a2d01d5921c845398a416299
김락현 <rlafkrgus963@kumoh.ac.kr>
2020-02-11 13:15:04 +0900

    [락현] S02P13D105-97 서버 기능 작성

---
+++ alarm-front-example/src/components/RoomDetail.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="container" v-cloak>
+    <div>
+      <h2>{{ room.name }}</h2>
+    </div>
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <label class="input-group-text">내용</label>
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        v-model="message"
+        @keyup.enter="sendMessage"
+      />
+      <div class="input-group-append">
+        <button class="btn btn-primary" type="button" @click="sendMessage">
+          보내기
+        </button>
+      </div>
+    </div>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="message in messages" :key="message">
+        {{ message.sender }} - {{ message.message }}
+      </li>
+    </ul>
+    <div></div>
+  </div>
+</template>
+
+<script>
+import sock from 'sockjs-client'
+import ws from 'webstomp-client'
+export default {
+  name: 'RoomDetail',
+  data () {
+    return {
+      roomId: '',
+      room: {},
+      sender: '',
+      message: '',
+      messages: []
+    }
+  },
+  created () {
+    this.roomId = localStorage.getItem('wschat.roomId')
+    this.sender = localStorage.getItem('wschat.sender')
+    this.findRoom()
+  },
+  methods: {
+    findRoom: function () {
+      axios.get('/chat/room/' + this.roomId).then(response => {
+        this.room = response.data
+      })
+    },
+    sendMessage: function () {
+      ws.send(
+        '/pub/chat/message',
+        {},
+        JSON.stringify({
+          type: 'TALK',
+          roomId: this.roomId,
+          sender: this.sender,
+          message: this.message
+        })
+      )
+      this.message = ''
+    },
+    recvMessage: function (recv) {
+      this.messages.unshift({
+        type: recv.type,
+        sender: recv.type == 'ENTER' ? '[알림]' : recv.sender,
+        message: recv.message
+      })
+    },
+    connect: function () {
+      ws.connect(
+        {},
+        function (frame) {
+          ws.subscribe('/sub/chat/room/' + vm.$data.roomId, function (message) {
+            var recv = JSON.parse(message.body)
+            vm.recvMessage(recv)
+          })
+          ws.send(
+            '/pub/chat/message',
+            {},
+            JSON.stringify({
+              type: 'ENTER',
+              roomId: vm.$data.roomId,
+              sender: vm.$data.sender
+            })
+          )
+        },
+        function (error) {
+          alert('error ' + error)
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style>
+[v-cloak] {
+  display: none;
+}
+</style>
